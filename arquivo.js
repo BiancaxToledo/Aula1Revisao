e05f950ba3fa29319c0f115d4f95b3d8c2efce81
BiancaxToledo <bianca.dharas2@gmail.com>
2019-08-07 20:12:50 -0300

    Revisão - TypeScript

---
+++ arquivo.js
@@ -0,0 +1,5 @@
+var messege = "hallo wolrd";
+console.log(messege);
+//let message: string;
+//message = "hallo world";
+//console.log(message);
---
+++ arquivo.ts
@@ -0,0 +1,6 @@
+let messege= "hallo wolrd";
+console.log(messege)
+
+//let message: string;
+//message = "hallo world";
+//console.log(message);
